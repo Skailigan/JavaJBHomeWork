6a91e4e865cd9ab0e8462341edd065cb9f7b781b
Katya Bu <Skailigan@gmail.com>
2017-05-08 17:47:43 +0300

    change "ShortSorting3Number"

--- src/com/company/ShortSorting3Numbers.java
+++ src/com/company/ShortSorting3Numbers.java
@@ -10,36 +10,34 @@ public class ShortSorting3Numbers {
         Scanner sc = new Scanner(System.in);
         System.out.println("Please enter first number:");
         String sFirstNumber = sc.nextLine();
-        int firstNumber = Integer.parseInt(sFirstNumber);
+        int x = Integer.parseInt(sFirstNumber);
         System.out.println("Please enter second number:");
         String sSecondNumber = sc.nextLine();
-        int secondNumber = Integer.parseInt(sSecondNumber);
+        int y = Integer.parseInt(sSecondNumber);
         System.out.println("Please enter third number:");
         String sThirdNumber = sc.nextLine();
-        int thirdNumber = Integer.parseInt(sThirdNumber);
-        System.out.println("You enter numbers:"+" "+firstNumber+";"+secondNumber+";"+thirdNumber+";");
+        int z    = Integer.parseInt(sThirdNumber);
+        System.out.println("You enter numbers:"+" "+x+";"+y+";"+z+";");
         System.out.println("Below is the sorting of numbers in descending order:");
-        int max,min = 0;
+        int temp = 0;
 
-        if (firstNumber>secondNumber){
-            max = firstNumber;
-            min = secondNumber;
+        if (z >= y){
+            temp = z;
+            z = y;
+            y = temp;
+        }
+
+        if (x >= y){
         }
         else {
-            max = secondNumber;
-            min = firstNumber;
-        }
-        if (max<thirdNumber){
-            max = thirdNumber;
-        }
-        else {
-            if(min>thirdNumber){
-                min = thirdNumber;
+            temp = x;
+            x = y;
+            y = temp;
             }
-        }
-        System.out.println("1: "+max);
-        System.out.println("2: "+((firstNumber+secondNumber+thirdNumber)-(max+min)));
-        System.out.println("3: "+min);
+
+        System.out.println("1: "+ x);
+        System.out.println("2: "+ y);
+        System.out.println("3: "+ z);
     }
 
 
